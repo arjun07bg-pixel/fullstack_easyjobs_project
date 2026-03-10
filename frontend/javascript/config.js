4f18922a12674d66b0386c88449054bd23651684
arjun07bg-pixel <arjun07.bg@gmail.com>
2026-03-10 19:35:49 +0530

    fix in config

--- frontend/javascript/config.js
+++ frontend/javascript/config.js
@@ -1,1 +1,2 @@
-export  const API_URL="https://fullstack-easyjobs-project.vercel.app/api";
+// export  const API_URL="https://fullstack-easyjobs-project.vercel.app/api";
+export  const API_URL="https://127.0.0.1:8000/api";
